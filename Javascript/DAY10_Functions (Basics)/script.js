5a036f131a3cce59cac0d798042bfd995be71274
vidyasagarnishad <160383448+vidyasagarnishad@users.noreply.github.com>
2025-08-03 11:49:00 +0530

    Add files via upload

---
+++ Javascript/DAY10_Functions (Basics)/script.js
@@ -0,0 +1,17 @@
+// DAY 10: Functions (Basics)
+
+// Function declaration
+function greetUser(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+greetUser("Vidyasagar"); // Hello, Vidyasagar!
+greetUser("JavaScript"); // Hello, JavaScript!
+
+// Function with return value
+function add(a, b) {
+  return a + b;
+}
+
+let sum = add(10, 5);
+console.log("Sum:", sum); // Sum: 15
